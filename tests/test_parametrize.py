902c7df0c8e3be23cafe3a66ebfc8892c87ef4d2
Roman Shelestov <m9icjkeee@gmail.com>
2023-08-22 16:35:43 +0400

    Созданы тесты с параметризацией и скипом

--- tests/test_parametrize.py
+++ tests/test_parametrize.py
@@ -2,16 +2,36 @@
 Переопределите параметр с помощью indirect параметризации на уровне теста
 """
 import pytest
+from selene import browser, be, have
+from selene.support.shared import config
+
+
+desktop = pytest.mark.parametrize('browser_parameters', [(1280, 960), (1920, 1280)], indirect=True)
+mobile = pytest.mark.parametrize('browser_parameters', [(820, 1180), (414, 896)], indirect=True)
 
 
 @pytest.fixture()
-def browser():
-    pass
+def browser_parameters(request):
+    config.timeout = 5
+    config.window_width = request.param[0]
+    config.window_height = request.param[1]
+    config.base_url = 'https://github.com/'
+    browser.quit()
 
 
-def test_github_desktop(browser):
-    pass
+@desktop
+def test_github_desktop(browser_parameters):
+    browser.open('')
+    browser.element('[href="/login"]').click()
+    assert browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
+    assert browser.element('#login_field').should(be.visible)
+    assert browser.element('#password').should(be.visible)
 
 
-def test_github_mobile(browser):
-    pass
+@mobile
+def test_github_mobile(browser_parameters):
+    browser.open('')
+    browser.element('.Button-content').click()
+    browser.element('.HeaderMenu-link--sign-in').click()
+    assert browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
+    assert browser.element('.js-sign-in-button').should(be.visible)
--- tests/test_skip.py
+++ tests/test_skip.py
@@ -1,18 +1,36 @@
-"""
-Параметризуйте фикстуру несколькими вариантами размеров окна
-Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
-"""
 import pytest
+from selene import browser, be, have
+from selene.support.shared import config
 
 
-@pytest.fixture()
-def browser():
-    pass
+def mobile_settings(width, height):
+    return width < height
 
 
-def test_github_desktop(browser):
-    pass
+@pytest.fixture(params=[(1280, 960), (1920, 1280), (820, 1180), (414, 896)])
+def browser_parameters(request):
+    config.timeout = 5
+    config.window_width = request.param[0]
+    config.window_height = request.param[1]
+    config.base_url = 'https://github.com/'
+    browser.quit()
 
 
-def test_github_mobile(browser):
-    pass
+def test_github_desktop(browser_parameters):
+    if mobile_settings(browser.config.window_width, browser.config.window_height):
+        pytest.skip('Разрешение только для мобильных тестов')
+    browser.open('')
+    browser.element('[href="/login"]').click()
+    assert browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
+    assert browser.element('#login_field').should(be.visible)
+    assert browser.element('#password').should(be.visible)
+
+
+def test_github_mobile(browser_parameters):
+    if not mobile_settings(browser.config.window_width, browser.config.window_height):
+        pytest.skip('Разрешение только для десктопных тестов')
+    browser.open('')
+    browser.element('.Button-content').click()
+    browser.element('.HeaderMenu-link--sign-in').click()
+    assert browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
+    assert browser.element('.js-sign-in-button').should(be.visible)
